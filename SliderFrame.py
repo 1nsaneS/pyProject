cbbf08a0647eb5b42d88dcf8011669340eb5b08b
1nsaneS <39540986+1nsaneS@users.noreply.github.com>
2018-07-18 14:56:28 +0800

    Revert "1"
    This reverts commit 4e45d94838b53e2578470cfca719449e84eb7545.

--- SliderFrame.py
+++ SliderFrame.py
@@ -6,7 +6,7 @@ class SliderFrame(wx.Frame):
 		wx.Frame.__init__(self,None,-1,'Slider Frame',
 			size=(300,350))
 		panel = wx.Panel(self,-1)
-		#self.count = 0
+		self.count = 0
 		slider = wx.Slider(panel,100,25,1,100,pos=(10,10),
 			size=(250,-1),
 			style=wx.SL_HORIZONTAL|wx.SL_AUTOTICKS|wx.SL_LABELS)
