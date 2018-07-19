47486fe2db97ec27d94c3f6e890b42b7ae4736ab
1nsaneS <39540986+1nsaneS@users.noreply.github.com>
2018-07-19 17:34:33 +0800

    RadioBox复选框

---
+++ RadioBox.py
@@ -0,0 +1,17 @@
+import wx
+
+class RadioBoxFrame(wx.Frame):
+	"""docstring for RadioBoxFrame"""
+	def __init__(self):
+		wx.Frame.__init__(self,None,-1,'RadioBoxFrame',
+			size=(350,200))
+		panel = wx.Panel(self,-1)
+		samplist = ('1','2','3','4','5')
+		wx.RadioBox(panel,-1,'33333',(10,10),wx.DefaultSize,
+			samplist,2,wx.RA_SPECIFY_COLS)
+
+if __name__ == '__main__':
+	app = wx.PySimpleApp()
+	RadioBoxFrame().Show()
+	app.MainLoop()
+		
